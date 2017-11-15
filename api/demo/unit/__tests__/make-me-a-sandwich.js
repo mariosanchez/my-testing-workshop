43a8da6782ea0d4dd578ed431fea7322705a42bd
Mario Sánchez <mr.mario.sm@gmail.com>
2017-11-15 20:26:46 +0100

    Introduction, challenge 1

--- api/demo/unit/__tests__/make-me-a-sandwich.js
+++ api/demo/unit/__tests__/make-me-a-sandwich.js
@@ -1,1 +1,19 @@
-test('todo', () => {})
+import makeMeASandwich from '../make-me-a-sandwich';
+
+test('returns null if the sandwitch does not exists', () => {
+  const req = {query: {}}
+  const result = makeMeASandwich(req)
+
+  expect(result).toBeNull()
+})
+
+test('returns my sandwitch', () => {
+  const sandwich = 'Monte Cristo'
+  const req = getReq(sandwich)
+  const result = makeMeASandwich(req)
+  expect(result).toBe(sandwich)
+})
+
+function getReq(sandwich) {
+  return {query: {sandwich}}
+}
