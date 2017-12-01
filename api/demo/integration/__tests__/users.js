86274fe9b4cc098dca544a2bd33ce17473951af0
Mario Sánchez <mr.mario.sm@gmail.com>
2017-12-01 13:07:01 +0100

    Use async await

--- api/demo/integration/__tests__/users.js
+++ api/demo/integration/__tests__/users.js
@@ -3,44 +3,33 @@ import startServer from '../start-server'
 
 let server
 
-beforeAll(() => {
-  return startServer().then(s => {
-    server = s
-  })
+beforeAll(async () => {
+  server = await startServer()
 })
 
 afterAll((done) => {
   server.close(done)
 })
 
-test('can get users', () => {
-  return axios.get('http://localhost:3001/api/users')
-  .then(response => {
-    const user = response.data.users[0]
-    expect(user).toMatchObject({
-      name: expect.any(String),
-      username: expect.any(String),
-    })
+test('can get users', async () => {
+  const response = await axios.get('http://localhost:3001/api/users')
+  const user = response.data.users[0]
+  expect(user).toMatchObject({
+    name: expect.any(String),
+    username: expect.any(String),
   })
 })
 
-test('can get 2 users at offset 3', () => {
-  const fiveUsersPromise = axios
+test('can get 2 users at offset 3', async () => {
+  const fiveUsers = await axios
     .get('http://localhost:3001/api/users?limit=5')
     .then(response => response.data.users)
-  const twoUsersPromise = axios
+  const twoUsers = await axios
     .get('http://localhost:3001/api/users?limit=2&offset=3')
     .then(response => response.data.users)
 
-  return Promise
-    .all([fiveUsersPromise, twoUsersPromise])
-    .then(responses => {
-      const [fiveUsers, twoUsers] = responses
-      const [firstUser, secondUser] = twoUsers
-      const [,,, firstUserAll, secondUserAll] = fiveUsers
-      expect(firstUser).toEqual(firstUserAll)
-      expect(secondUser).toEqual(secondUserAll)
-    })
-
-
+  const [firstUser, secondUser] = twoUsers
+  const [,,, firstUserAll, secondUserAll] = fiveUsers
+  expect(firstUser).toEqual(firstUserAll)
+  expect(secondUser).toEqual(secondUserAll)
 })
