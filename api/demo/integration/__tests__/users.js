29eddeaf401877fed8a003df59a41e8021e2d6dd
Mario Sánchez <mr.mario.sm@gmail.com>
2017-12-01 13:06:31 +0100

    Integration tests

--- api/demo/integration/__tests__/users.js
+++ api/demo/integration/__tests__/users.js
@@ -1,1 +1,46 @@
-test('todo', () => {})
+import axios from 'axios'
+import startServer from '../start-server'
+
+let server
+
+beforeAll(() => {
+  return startServer().then(s => {
+    server = s
+  })
+})
+
+afterAll((done) => {
+  server.close(done)
+})
+
+test('can get users', () => {
+  return axios.get('http://localhost:3001/api/users')
+  .then(response => {
+    const user = response.data.users[0]
+    expect(user).toMatchObject({
+      name: expect.any(String),
+      username: expect.any(String),
+    })
+  })
+})
+
+test('can get 2 users at offset 3', () => {
+  const fiveUsersPromise = axios
+    .get('http://localhost:3001/api/users?limit=5')
+    .then(response => response.data.users)
+  const twoUsersPromise = axios
+    .get('http://localhost:3001/api/users?limit=2&offset=3')
+    .then(response => response.data.users)
+
+  return Promise
+    .all([fiveUsersPromise, twoUsersPromise])
+    .then(responses => {
+      const [fiveUsers, twoUsers] = responses
+      const [firstUser, secondUser] = twoUsers
+      const [,,, firstUserAll, secondUserAll] = fiveUsers
+      expect(firstUser).toEqual(firstUserAll)
+      expect(secondUser).toEqual(secondUserAll)
+    })
+
+
+})
