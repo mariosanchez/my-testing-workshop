d7e90a51a4b39fe37410da63a9f8e2cd642b2ae3
Mario Sánchez <mr.mario.sm@gmail.com>
2018-01-22 13:36:21 +0100

    Client (React) testing with snapshots

--- client/demo/unit/__tests__/toggle.js
+++ client/demo/unit/__tests__/toggle.js
@@ -1,1 +1,44 @@
-test('todo', () => {})
+import React from 'react'
+import {mount, render} from 'enzyme'
+import Toggle from '../toggle'
+
+test('the component renders with defaults', () => {
+  const wrapper = renderToggle()
+  expect(wrapper).toMatchSnapshotWithGlamor()
+})
+test('onToggle function is call when button is clicked', () => {
+  const onToggle = jest.fn()
+  const wrapper = mountToggle(
+    {
+      onToggle,
+    },
+  )
+  const button = wrapper.find(`[data-test="button"]`)
+  expect(wrapper).toMatchSnapshotWithGlamor('1. Before toogle')
+  button.simulate('click')
+  expect(wrapper).toMatchSnapshotWithGlamor('2. After toogle')
+  expect(onToggle).toHaveBeenCalledTimes(1)
+  expect(onToggle).toHaveBeenCalledWith(true)
+})
+
+function renderToggle(props = {}) {
+  const propsToUser = Object.assign(
+    {
+      onToggle() {},
+      children: 'Child',
+    },
+    props,
+  )
+  return render(<Toggle {...propsToUser} />)
+}
+
+function mountToggle(props = {}) {
+  const propsToUser = Object.assign(
+    {
+      onToggle() {},
+      children: 'Child',
+    },
+    props,
+  )
+  return mount(<Toggle {...propsToUser} />)
+}
